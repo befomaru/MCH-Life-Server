82db0227294d17d6835572aed29773a23d03f48b
befomaru <32425756+befomaru@users.noreply.github.com>
2018-07-22 12:45:42 +0000

    がばがば修正
    通りすがったので適当に修正してみました。動作は保証しません、ごめんなさい。

--- NoName/src/machi/main.php
+++ NoName/src/machi/main.php
@@ -4,12 +4,17 @@ namespace machi;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 
 class Main extends PluginBase implements Listener{
-
+	
+	
+        public $sendBool = false;
+	
+	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	    $this->getLogger()->info("§a[起動] §bNoName§aを起動しました。");
@@ -21,28 +26,32 @@ class Main extends PluginBase implements Listener{
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-		if(!$sender instanceof Player) {
+		
+		if($sender instanceof ConsoleCommandSender) {
 			$this->getLogger()->warning("§cゲーム内で実行して下さい。");
-			return true;
+			return false;
 		}
 		switch (strtolower($command->getName())) {
 			case "noname":
-			$player = $this->getServer()->getOnlinePlayers();
-			if ($sender->getNameTag() === "") {
-				foreach ($player as $player) {
+			$players = $this->getServer()->getOnlinePlayers();
+			if(!$this->sendBool){
+				foreach ($players as $player) {
 					$this->getLogger()->info("§l§bNoName §aが§e無効§aになりました。");
 					$player->sendMessage("§l§bＮｏＮａｍｅ §aが§e無効§aになりました。");
 					$player->sendMessage("");
 					$player->sendMessage("§l§b称号§aは元に戻っていません。");
 					$player->sendMessage("§l§b称号§aを表示させるには§eリログ§aする必要があります。");
 					$player->setNameTag($player->getName());
+					
 				}
+				$this->sendBool = true;
 			}else {
-				foreach ($player as $player) {
+				foreach ($players as $player) {
 					$this->getLogger()->info("§l§bNoName §aが§e有効§aになりました。");
 					$player->sendMessage("§l§bＮｏＮａｍｅ §aが§e有効§aになりました。");
 					$player->setNameTag("");
 				}
+				$this->sendBool = false;
 			}
 			break;
 			
